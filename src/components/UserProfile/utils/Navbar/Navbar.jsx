06360fa2563e202349234a87acf1260198017324
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-06-24 10:29:18 +0100

    Navbar is fully responsive

--- src/components/UserProfile/utils/Navbar/Navbar.jsx
+++ src/components/UserProfile/utils/Navbar/Navbar.jsx
@@ -31,10 +31,33 @@ const navbarItems = [
     },
 ];
 
+const LittleNav = ({ activeLink, setActiveLink, setViewLittleNav }) => {
+    return (
+        <div className='little-navbar'>
+            {
+                navbarItems.slice(3, 6).map((item, i) => (
+                    <Link 
+                        key={ `link-${ i+1 }` }
+                        className={ activeLink === item.link ? 'navlink current' : 'navlink' }
+                        to={ item.link }
+                        onClick={ () => {
+                            setActiveLink(item.link);
+                            setViewLittleNav(false); 
+                        }}
+                    >
+                        { item.title }
+                    </Link>
+                ))
+            }
+        </div>
+    );
+};
+
 
 
 const Navbar = () => {
     const [activeLink, setActiveLink] = useState('');
+    const [viewLittleNav, setViewLittleNav] = useState(false);
     const location = useLocation();
 
     useEffect(() => {
@@ -51,16 +74,31 @@ const Navbar = () => {
                 navbarItems.map((item, i) => (
                     <Link 
                         key={ `link-${ i+1 }` }
-                        className={ activeLink === item.link ? 'navlink active' : 'navlink' }
+                        className={ activeLink === item.link ? `navlink active ${ item.link }` : `navlink ${ item.link }` }
                         to={ item.link }
+                        onClick={ () => setViewLittleNav(false) }
                     >
                         { item.title }
                     </Link>
                 ))
             }
-            <div className='navlink plus'>
+            <div 
+                className={ 
+                    (activeLink === 'photos') || (activeLink === 'videos') || (activeLink === 'reels') ?
+                    'navlink plus active' : 'navlink plus' 
+                }
+                onClick={ () => setViewLittleNav(!viewLittleNav) }
+            >
                 Plus
                 <TbTriangleInvertedFilled size={ 10 } />
+
+                { viewLittleNav && (
+                    <LittleNav
+                        activeLink={ activeLink }
+                        setActiveLink={ setActiveLink }
+                        setViewLittleNav={ setViewLittleNav }
+                    />
+                )}
             </div>
             <div className='navlink dots'>
                 <BsThreeDots size={ 20 } />
