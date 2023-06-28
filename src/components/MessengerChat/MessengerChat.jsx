aa208896a171891cc083a2c5d8ae82d7d5155113
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-06-28 19:05:33 +0100

    Messages are added to firebase based on the senderLink and receiverLink

--- src/components/MessengerChat/MessengerChat.jsx
+++ src/components/MessengerChat/MessengerChat.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
-import { getDoc, doc } from 'firebase/firestore';
-import { IoClose } from 'react-icons/io5';
+import { getDoc, doc, addDoc, collection, query, where, and, updateDoc, arrayUnion, getDocs, or } from 'firebase/firestore';
+import { IoClose, IoSend } from 'react-icons/io5';
 
 import { db } from '../../config/firebase';
 import { useAuth } from '../../context/AuthContext';
@@ -12,6 +12,8 @@ import images from '../../constants/images';
 
 
 const MessengerChat = () => {
+    const inputMessageRef = useRef(null);
+    const [message, setMessage] = useState('');
     const { user } = useAuth();
     const [userInfo, setUserInfo] = useState({});
     const receiverInfo = useLoaderData();
@@ -32,7 +34,66 @@ const MessengerChat = () => {
         catch(err) {
             console.error(err);
         };
-    }
+    };
+
+    useEffect(() => {
+    }, [message]);
+
+    const autoResize = (ref) => {
+        const current = ref.current;
+        current.style.height = 'auto';
+        current.style.height = `${ current.scrollHeight }px`;
+    };
+
+    const handleSendMessage = () => {
+        const q = query(
+            collection(db, 'chats'),
+            or(
+                and(
+                    where('senderLink', '==', userInfo.userLink),
+                    where('receiverLink', '==', receiverInfo.userLink),
+                ),
+                and(
+                    where('senderLink', '==', receiverInfo.userLink),
+                    where('receiverLink', '==', userInfo.userLink),
+                )
+            )
+        );
+
+        getDocs(q)
+            .then((data) => {
+                if (!data.empty) {
+                    updateDoc(doc(db, 'chats', data.docs[0].id), {
+                        messages: arrayUnion({
+                            message: message,
+                            sender: userInfo.userLink,
+                            time: new Date().getTime(),
+                        })
+                    })
+                        .then(() => {
+                            setMessage('');
+                            console.log('Messages updated');
+                        })
+                        .catch((err) => console.log('1) => ', err));
+                } else {
+                    addDoc(collection(db, 'chats'), {
+                        senderLink: userInfo.userLink,
+                        receiverLink: receiverInfo.userLink,
+                        messages: [{
+                            message: message,
+                            sender: userInfo.userLink,
+                            time: new Date().getTime(),
+                        }],
+                    })
+                        .then(() => {
+                            setMessage('');
+                            console.log('Message added');
+                        })
+                        .catch((err) => console.log('2) => ', err));
+                }
+            })
+            .catch((err) => console.log('3) => ', err));
+    };
 
     return (
         <div className='messenger-chat-container'>
@@ -60,9 +121,29 @@ const MessengerChat = () => {
                     </Link>
                 </div>
                 <div className='messages-container'>
-                    <div className='messages-container__messages'></div>
+                    <div className='messages-container__messages'>
+
+                    </div>
                 </div>
-                <div className='footer'></div>
+                <div className='footer'>
+                    <div className='message-container'>
+                        <textarea
+                            ref={ inputMessageRef }
+                            className='message'
+                            onChange={ (e) => {
+                                setMessage(e.target.value);
+                                autoResize(inputMessageRef);
+                            }}
+                            value={ message }
+                        ></textarea>
+                    </div>
+                    <button
+                        disabled={ message === '' }
+                        onClick={ () => handleSendMessage() }
+                    >
+                        <IoSend size='22' />
+                    </button>
+                </div>
             </div>
         </div>
     );
