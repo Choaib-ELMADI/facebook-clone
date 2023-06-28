dac7f64cea978dcda05d3e9d56a9aa24e16d3f19
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-06-28 14:28:31 +0100

    Implement the messenger structure

--- src/components/MessengerChat/MessengerChat.jsx
+++ src/components/MessengerChat/MessengerChat.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import { getDoc, doc } from 'firebase/firestore';
+import { IoClose } from 'react-icons/io5';
 
 import { db } from '../../config/firebase';
 import { useAuth } from '../../context/AuthContext';
 import { Navbar } from '../../components/index';
 import './MessengerChat.scss';
+import images from '../../constants/images';
 
 
 
@@ -36,8 +38,31 @@ const MessengerChat = () => {
         <div className='messenger-chat-container'>
             <Navbar />
             <div className='messenger-chat-container__content'>
-                <h3>Sender: { userInfo.userLink }</h3>
-                <h3>Receiver: { receiverInfo.userLink }</h3>
+                <div className='header'>
+                    <Link 
+                        className='receiver-profile'
+                        to={ `/users/${ receiverInfo.userLink }` }
+                    >
+                        <img 
+                            src={ receiverInfo.userProfile ? receiverInfo.userProfile : images.user_1 } 
+                            alt=""
+                            draggable='false'
+                            loading='lazy'
+                            referrerPolicy='no-referrer'
+                        />
+                        <p>{ receiverInfo.userName ? receiverInfo.userName : 'User' }</p>
+                    </Link>
+                    <Link 
+                        to='/'
+                        className='link-to-home'
+                    >
+                        <IoClose size={ 30 } />
+                    </Link>
+                </div>
+                <div className='messages-container'>
+                    <div className='messages-container__messages'></div>
+                </div>
+                <div className='footer'></div>
             </div>
         </div>
     );
