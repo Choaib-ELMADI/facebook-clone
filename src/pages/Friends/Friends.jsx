72d6060035a66adf77fda2c8a3b190e27fa5ca74
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-07-01 00:43:50 +0100

    Friends section is done

--- src/pages/Friends/Friends.jsx
+++ src/pages/Friends/Friends.jsx
@@ -1,14 +1,59 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import './Friends.scss';
 import { Navbar, Sidebar } from '../../components/index';
 
+const Friend = ({ user, handleDelete }) => {
+  return (
+    <div className='friend-wrapper'>
+      <div className='friend-profile'>
+        <img 
+          src={ user.picture.large } 
+          alt=""
+          draggable='false'
+          loading='lazy'
+        />
+      </div>
+      <p className='friend-username'>{ user.name.first } { user.name.last }</p>
+      <button className='add'>Ajouter comme ami(e)</button>
+      <button 
+        className='close'
+        onClick={ () => handleDelete(user.email) }
+      >Supprimer</button>
+    </div>
+  );
+};
+
 
 
 const Friends = () => {
+  const [users, setUsers] = useState([]);
+
   useEffect(() => {
     document.title = 'Friends | Facebook';
   }, []);
+
+  useEffect(() => {
+    const fetchUserProfiles = async () => {
+      try {
+        const response = await fetch('https://randomuser.me/api/?results=10');
+        const data = await response.json();
+        setUsers(data.results);
+      } catch (error) {
+        console.error('Error fetching user profiles:', error);
+      }
+    };
+
+    fetchUserProfiles();
+  }, []);
+
+  const handleDelete = (email) => {
+    setUsers(
+      users.filter(
+        user => user.email !== email
+      )
+    );
+  };
 
   return (
     <main className="friends">
@@ -16,11 +61,31 @@ const Friends = () => {
       <div className='section'>
           <Sidebar title='Amis' />
           <div className='friends-content'>
-            <p>Friend</p>
-            <p>Friend</p>
-            <p>Friend</p>
-            <p>Friend</p>
-            <p>Friend</p>
+          {
+            users.length < 1 ?
+            <>
+              <div className='friend-model'>
+                <div className='profile'>No users</div>
+                <div className='username' />
+                <div className='btn add' />
+                <div className='btn close' />
+              </div> 
+              <div className='friend-model'>
+                <div className='profile'>No users</div>
+                <div className='username' />
+                <div className='btn add' />
+                <div className='btn close' />
+              </div>
+            </>
+            :
+            users.map((user, i) => (
+              <Friend 
+                key={ `frined-${ i+1 }` } 
+                user={ user } 
+                handleDelete={ handleDelete }
+              />
+            ))
+          }
           </div>
       </div>
     </main>
