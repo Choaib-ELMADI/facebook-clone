c0ddb279b37716cee60649cee01d0a0e4dabf6e3
Choaib ELMADI <choaib3elmadi@gmail.com>
2023-06-30 20:14:38 +0100

    Change some styles

--- src/pages/Bookmarks/Bookmarks.jsx
+++ src/pages/Bookmarks/Bookmarks.jsx
@@ -1,11 +1,32 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import { query, collection, where, getDocs } from 'firebase/firestore';
 
 import './Bookmarks.scss';
 import { LeftSidebar, Navbar } from '../../components/index';
+import { db } from '../../config/firebase';
+import { useAuth } from '../../context/AuthContext';
 
 
 
 const Bookmarks = () => {
+    const { user } = useAuth();
+    const [userInfo, setUserInfo] = useState({});
+
+    useEffect(() => {
+        fetchUserInfo();
+    }, [user]);
+    
+    const fetchUserInfo = () => {
+        const q = query(collection(db, 'users'), where('userId', '==', user.uid));
+        getDocs(q)
+            .then(data => {
+                data.forEach(d => {
+                    setUserInfo({ ...d.data() })
+                })
+            })
+            .catch(err => console.error(err));
+    };
+
     useEffect(() => {
         document.title = 'Bookmarks | Facebook';
     }, []);
@@ -13,7 +34,7 @@ const Bookmarks = () => {
     return (
         <main className="bookmarks">
             <Navbar />
-            <LeftSidebar />
+            <LeftSidebar userInfo={ userInfo } />
         </main>
     );
 };
